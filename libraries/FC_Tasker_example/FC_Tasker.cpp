a365354c9e3fc81d6dfcf6efbb35601036e3e5f2
Janek <jan.wielgus12@gmail.com>
2018-12-28 16:25:45 +0100

    FC_Tasker structure remaked
    Previous class is now divided by two different classes:
    - SimpleTasker - uses only micros() and calculate elapsed time
    - Tasker - uses Hardware Timer to handle one main task and inherit form SimpleTasker to execute other tasks

--- libraries/FC_Tasker_example/FC_Tasker.cpp
+++ libraries/FC_Tasker_example/FC_Tasker.cpp
@@ -4,56 +4,31 @@
 
 #include "FC_Tasker.h"
 
-
-volatile bool FC_Tasker::baseLoopFlag = false;
-
-// Now do not used
-//volatile uint32_t FC_Tasker::baseLoopCounter = 0;
-//volatile uint32_t FC_Tasker::mainLoopCounter = 0;
+//////////////////////////////////////////////////////////////////////////
+//                FC_SimpleTasker        \/  \/  \/
+//////////////////////////////////////////////////////////////////////////
 
 
-void baseLoopTimerHandler()
+FC_SimpleTasker::FC_SimpleTasker()
 {
-	FC_Tasker::baseLoopFlag = true;
-	//FC_Tasker::baseLoopCounter++; // increment the counter
-	//if ((FC_Tasker::baseLoopCounter % 4) == 0) FC_Tasker::mainLoopCounter++; // ONLY IF MAIN LOOP IS 250Hz AND BASE_INTERVAL IS 500 !!!!!!!!!!! - this part have to be implemented in a better way 
+	// nothing
 }
 
 
-FC_Tasker::FC_Tasker( void (*mainFuncPointer)(), long interv, uint16_t maxDur ) : BASE_INTERVAL(interv)
-{
-	/*
-	Timer2.setMode(TIMER_CH1, TIMER_OUTPUTCOMPARE);
-	Timer2.setPeriod(BASE_INTERVAL); // in microseconds
-	Timer2.setCompare(TIMER_CH1, 1);      // overflow might be small ???
-	Timer2.attachInterrupt(TIMER_CH1, baseLoopTimerHandler);
-	*/
-	
-	timer2 = new HardwareTimer(2);
-	timer2->pause();
-	timer2->setPeriod(BASE_INTERVAL);
-	timer2->setChannel1Mode(TIMER_OUTPUT_COMPARE);
-	timer2->setCompare(TIMER_CH1, 1);
-	timer2->attachCompare1Interrupt(baseLoopTimerHandler);
-	timer2->refresh();
-	timer2->resume();
-}
-
-
-FC_Tasker::~FC_Tasker()
+FC_SimpleTasker::~FC_SimpleTasker()
 {
 	if (amtOfTasks > 0)
-		delete [] taskList;
+	delete [] taskList;
 }
 
 
 // Add a new task at the end of dynamically created array
-void FC_Tasker::addFunction( void (*funcPointer)(), long interv, uint16_t maxDur )
+void FC_SimpleTasker::addFunction( void (*funcPointer)(), long interv, uint16_t maxDur )
 {
 	// check if there is too much tasks
 	if (amtOfTasks+1 > MAX_AMT_OF_TASKS)
-		return;
-		
+	return;
+	
 	amtOfTasks++;
 	
 	// create new tasks container
@@ -72,6 +47,114 @@ void FC_Tasker::addFunction( void (*funcPointer)(), long interv, uint16_t maxDur
 	taskList = newTaskList;
 }
 
+
+void FC_SimpleTasker::scheduleTasks()
+{	
+	for (int i=0; i<amtOfTasks; i++)
+	{
+		for (int j=i+1; j<amtOfTasks; j++)
+		{
+			int bigger = max(taskList[i].interval+taskList[i].shift, taskList[j].interval+taskList[j].shift);
+			int smaller = min(taskList[i].interval+taskList[i].shift, taskList[j].interval+taskList[j].shift);
+			
+			if (bigger % smaller == 0) // is divisible
+				taskList[j].shift += taskList[i].maxDuration + TIME_SHIFT_BASE; // calculate new shift for second task
+		}
+	}
+}
+
+
+void FC_SimpleTasker::runTasker()
+{
+	for (uint8_t i=0; i<amtOfTasks; i++)
+	{
+		static uint32_t curTime = 0; // time now
+		
+		curTime = micros();
+		
+		//if time has elapsed -> execute the task
+		if (curTime > (taskList[i].lastExecuteTime + taskList[i].interval + taskList[i].shift))
+		{
+			taskList[i].lastExecuteTime = curTime;
+			(*taskList[i].functionPointer)();
+		}
+	}
+}
+
+
+// Copy one Tasks array to another
+void FC_SimpleTasker::copyTaskList(Task *from, Task *to, uint8_t amount)
+{
+	for (int i=0; i < amount; i++)
+	{
+		to->functionPointer = from->functionPointer;
+		to->interval = from->interval;
+		to->maxDuration = from->maxDuration;
+		to->lastExecuteTime = from->lastExecuteTime;
+		to->shift = from->shift;
+	}
+}
+
+/*
+bool FC_SimpleTasker::checkIfContain(Task** source, int amt, Task* toCheck)
+{
+	for (int i=0; i<amt; i++)
+	{
+		if (source[i]->functionPointer == toCheck->functionPointer)
+			return true;
+	}
+	return false;
+}
+*/
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////
+//                FC_Tasker        \/  \/  \/
+//////////////////////////////////////////////////////////////////////////
+
+
+volatile bool FC_Tasker::baseLoopFlag = false;
+
+// Now do not used
+//volatile uint32_t FC_Tasker::baseLoopCounter = 0;
+//volatile uint32_t FC_Tasker::mainLoopCounter = 0;
+
+
+void baseLoopTimerHandler()
+{
+	FC_Tasker::baseLoopFlag = true;
+	//FC_Tasker::baseLoopCounter++; // increment the counter
+	//if ((FC_Tasker::baseLoopCounter % 4) == 0) FC_Tasker::mainLoopCounter++; // ONLY IF MAIN LOOP IS 250Hz AND BASE_INTERVAL IS 500 !!!!!!!!!!! - this part have to be implemented in a better way 
+}
+
+
+FC_Tasker::FC_Tasker( void (*mainFuncPointer)(), long interv, uint16_t maxDur ) : BASE_INTERVAL(interv), FC_SimpleTasker()
+{
+	/*
+	Timer2.setMode(TIMER_CH1, TIMER_OUTPUTCOMPARE);
+	Timer2.setPeriod(BASE_INTERVAL); // in microseconds
+	Timer2.setCompare(TIMER_CH1, 1);      // overflow might be small ???
+	Timer2.attachInterrupt(TIMER_CH1, baseLoopTimerHandler);
+	*/
+	
+	mainTask.functionPointer = mainFuncPointer;
+	mainTask.interval = interv;
+	mainTask.maxDuration = maxDur;
+	
+	timer2 = new HardwareTimer(2); // Hardware Timer 2 (STM32 BluePill)
+	timer2->pause();
+	timer2->setPeriod(BASE_INTERVAL);
+	timer2->setChannel1Mode(TIMER_OUTPUT_COMPARE);
+	timer2->setCompare(TIMER_CH1, 1);
+	timer2->attachCompare1Interrupt(baseLoopTimerHandler);
+	timer2->refresh();
+	timer2->resume();
+}
+
+
 /* - now in the constructor
 // Executed once. Add the main task
 void FC_Tasker::addMainFunction( void (*mainFuncPointer)(), long interv, uint16_t maxDur )
@@ -82,77 +165,7 @@ void FC_Tasker::addMainFunction( void (*mainFuncPointer)(), long interv, uint16_
 }
 */
 
-void FC_Tasker::scheduleTasks()
-{
-	/*
-		Trzeba sprawdzic kazde z kazdym i zobaczyc czy interval nie jest wielokrotnoscia (czyli czy % == 0)
-	*/
-	
-	// create array of bools to know if for certain task shift have been already set
-	bool* isShiftSet = new bool[amtOfTasks]; // true - shift is set for that function and other with the same interval
-	for (int i=0; i<amtOfTasks; i++) isShiftSet = false;
-	
-	for (int i=0; i<amtOfTasks; i++)
-	{
-		// if shift for that interval have been already set -> skip
-		if (isShiftSet[i] == true)
-			continue;
-			
-		int smallerInt; // created there to use in the next for loop
-		
-		// array of pointers to the tasks with the same interval
-		Task** sameIntTasks = new Task*[amtOfTasks];
-		int amtOfSameIntTasks = 0;
-		
-		// look for and add to the sameIntTasks array tasks which have the same interval
-		for (int j=i+1; j<amtOfTasks; j++)
-		{
-			// if shift for that interval have been already set -> skip
-			if (isShiftSet[j] == true)
-				continue;
-			
-			// Bigger and smaller interval
-			smallerInt = min(taskList[i].interval, taskList[j].interval);
-			int biggerInt = max(taskList[i].interval, taskList[j].interval);
-			
-			if (biggerInt % smallerInt == 0) // If is divisible
-			{
-				/* checking this is meaningless because 'i' is always different than 'j', and in one i iteration there can't be the same two functions. isShiftSet array is checked at the beginning of the both 'i' and 'j'
-				// if not contain then add
-				if (checkIfContain(sameIntTasks, amtOfSameIntTasks, &(taskList[i])) == false)
-					sameIntTasks[amtOfSameIntTasks++] = &taskList[i];
-				if (checkIfContain(sameIntTasks, amtOfSameIntTasks, &(taskList[j])) == false)
-					sameIntTasks[amtOfSameIntTasks++] = &taskList[j];
-				*/
-				if (!isShiftSet[i]) // 'i' is the same for one 'j' loop so it must be checked to to avoid duplicates
-					sameIntTasks[amtOfSameIntTasks++] = &taskList[i];
-				sameIntTasks[amtOfSameIntTasks++] = &taskList[j]; // 'j' is always new
-					
-				isShiftSet[i] = true;
-				isShiftSet[j] = true;
-			}
-		}
-		
-		
-		// if there were zero tasks with the same interval -> skip
-		if(amtOfSameIntTasks < 2)
-		{
-			isShiftSet[i] = true;
-			continue;
-		}
-		
-		//calculate the shift for tasks with the same interval
-		float baseShift = float(smallerInt)/amtOfSameIntTasks; // eg.: =5 then shift1=5, shift2=10, shift3=15...
-		for (int q=1; q<amtOfSameIntTasks; q++)
-		{
-			sameIntTasks[q]->shift = uint16_t(q * baseShift); // calculate the next shifts
-		}
-		
-		delete [] sameIntTasks;
-	}
-	
-	delete [] isShiftSet;
-}
+
 
 
 void FC_Tasker::runTasker()
@@ -181,44 +194,7 @@ void FC_Tasker::runTasker()
 		Reszte zadan ma byc wywolywane na podstawie czasu jaki uplynal od ostatniego wykonania, uzywajac micros() i dodajac shift
 	*/
 	
-	for (uint8_t i=0; i<amtOfTasks; i++)
-	{
-		static uint32_t curTime = 0; // time now
-		
-		curTime = micros();
-		
-		//if time has elapsed -> execute the task
-		if (curTime > (taskList[i].lastExecuteTime + taskList[i].interval + taskList[i].shift))
-		{
-			taskList[i].lastExecuteTime = curTime;
-			(*taskList[i].functionPointer)();
-		}
-	}
+	FC_SimpleTasker::runTasker();
 }
 
-
-// Copy one Tasks array to another
-void FC_Tasker::copyTaskList(Task *from, Task *to, uint8_t amount)
-{
-	for (int i=0; i < amount; i++)
-	{
-		to->functionPointer = from->functionPointer;
-		to->interval = from->interval;
-		to->maxDuration = from->maxDuration;
-		to->lastExecuteTime = from->lastExecuteTime;
-		to->shift = from->shift;
-	}
-}
-
-/*
-bool FC_Tasker::checkIfContain(Task** source, int amt, Task* toCheck)
-{
-	for (int i=0; i<amt; i++)
-	{
-		if (source[i]->functionPointer == toCheck->functionPointer)
-			return true;
-	}
-	return false;
-}
-*/
 
