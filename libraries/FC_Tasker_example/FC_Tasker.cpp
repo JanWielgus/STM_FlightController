f8db3657a1d2e41fab467b307b14832768fb2239
JanWielgus <jan.wielgus12@gmail.com>
2018-12-29 18:13:08 +0100

    Feat/first idea test (dev of FC_Tasker) (#4)
    * First remake
    
    * Upgrades in handling the secondary tasks
    
    scheduleTasks() method in progress...
    
    * Base code is ready for testing
    
    Base code is prepared to the first compiling and debugging
    
    * First quick error debugging
    
    * Code is compilable, redy for testing on the microcontroller
    
    * Some changes after first testing. STM gets stuck after upload
    
    * Update The new thinking of my flight controller (project).txt
    
    * Next repairs after in-acction testing
    
    - One '-' is deleted
    - Commented meningless checking in scheduleTasks()
    - Small change is pointer usage
    
    * Bug fix after arduino tests
    
    '>' instead of '<'
    
    * Update The new thinking of my flight controller (project).txt
    
    Tasker structure remaked: divided into two classes:
    - SimpleTasker - do not use any hardware Timers, only micros() and elapsed time
    - Tasker - (inherits from SimpleTasker) use hardware Timer to execute main task, other tasks like in SimpleTasker
    
    * First idea test/feat/FC_Tasker structure remake (#3)
    
    * FC_Tasker structure remaked
    
    Previous class is now divided by two different classes:
    - SimpleTasker - uses only micros() and calculate elapsed time
    - Tasker - uses Hardware Timer to handle one main task and inherit form SimpleTasker to execute other tasks
    
    * Variable rename
    
    In Task structure shift on timeShift
    
    * Small improve in time shift calculations for testing
    
    * Few fixes and changes after tests, now it works!
    
    * Update FC_Tasker_example.cppproj

--- libraries/FC_Tasker_example/FC_Tasker.cpp
+++ libraries/FC_Tasker_example/FC_Tasker.cpp
@@ -4,56 +4,158 @@
 
 #include "FC_Tasker.h"
 
+//////////////////////////////////////////////////////////////////////////
+//                FC_SimpleTasker        \/  \/  \/
+//////////////////////////////////////////////////////////////////////////
 
-volatile bool FC_Tasker::baseLoopFlag = false;
-volatile uint32_t FC_Tasker::baseLoopCounter = 0;
-volatile uint32_t FC_Tasker::mainLoopCounter = 0;
 
-
-void baseLoopTimerHandler()
+FC_SimpleTasker::FC_SimpleTasker()
 {
-	//FC_Tasker::baseLoopFlag = true;  // idk if it will be used if there is baseLoopCounter
-	FC_Tasker::baseLoopCounter++; // increment the counter
-	if ((FC_Tasker::baseLoopCounter % 4) == 0) FC_Tasker::mainLoopCounter++; // ONLY IF MAIN LOOP IS 250Hz AND BASE_INTERVAL IS 500 !!!!!!!!!!! - this part have to be implemented in a better way 
+	// nothing
 }
 
 
-FC_Tasker::FC_Tasker(uint16_t base_interval) : BASE_INTERVAL(base_interval)
+FC_SimpleTasker::~FC_SimpleTasker()
 {
-	Timer2.setMode(TIMER_CH1, TIMER_OUTPUTCOMPARE);
-	Timer2.setPeriod(BASE_INTERVAL); // in microseconds
-	Timer2.setCompare(TIMER_CH1, 1);      // overflow might be small ???
-	Timer2.attachInterrupt(TIMER_CH1, baseLoopTimerHandler);
-}
-
-
-FC_Tasker::~FC_Tasker()
-{
-	if (numberOfTasks > 0)
-		delete [] taskList;
+	if (amtOfTasks > 0)
+	delete [] taskList;
 }
 
 
 // Add a new task at the end of dynamically created array
-void FC_Tasker::addFunction( void (*funcPointer)(), long interv, uint16_t maxDur )
+void FC_SimpleTasker::addFunction( void (*funcPointer)(), long interv, uint16_t maxDur )
 {
-	numberOfTasks++;
+	// check if there is too much tasks
+	if (amtOfTasks+1 > MAX_AMT_OF_TASKS)
+	return;
+	
+	amtOfTasks++;
 	
 	// create new tasks container
-	Task * newTaskList = new Task[numberOfTasks];
+	Task * newTaskList = new Task[amtOfTasks];
 
 	// copy old tasks to the new container and delete old one
-	copyTaskList(taskList, newTaskList, numberOfTasks-1);
+	copyTaskList(taskList, newTaskList, amtOfTasks);
 	delete [] taskList;
 	
 	// add new task at the end
-	newTaskList[numberOfTasks-1].functionPointer = funcPointer;
-	newTaskList[numberOfTasks-1].interval = interv;
-	newTaskList[numberOfTasks-1].maxDuration = maxDur;
+	newTaskList[amtOfTasks-1].functionPointer = funcPointer;
+	newTaskList[amtOfTasks-1].interval = interv;
+	newTaskList[amtOfTasks-1].maxDuration = maxDur;
+	newTaskList[amtOfTasks-1].lastExecuteTime = 0;
+	newTaskList[amtOfTasks-1].timeShift = 0;
 	taskList = newTaskList;
 }
 
 
+void FC_SimpleTasker::scheduleTasks()
+{	
+	for (int i=0; i<amtOfTasks; i++)
+	{
+		for (int j=i+1; j<amtOfTasks; j++)
+		{
+			int bigger = max(taskList[i].interval+taskList[i].timeShift, taskList[j].interval+taskList[j].timeShift);
+			int smaller = min(taskList[i].interval+taskList[i].timeShift, taskList[j].interval+taskList[j].timeShift);
+			
+			if (bigger % smaller == 0) // is divisible
+				taskList[j].timeShift += int(taskList[i].interval/3) + taskList[i].maxDuration + TIME_SHIFT_BASE; // calculate new shift for second task, it's not ideal, but works
+		}
+	}
+}
+
+
+void FC_SimpleTasker::runTasker()
+{
+	for (uint8_t i=0; i<amtOfTasks; i++)
+	{
+		static uint32_t curTime = 0; // time now
+		
+		curTime = micros();
+		
+		//if time has elapsed -> execute the task
+		if (curTime > (taskList[i].lastExecuteTime + taskList[i].interval + taskList[i].timeShift))
+		{
+			taskList[i].lastExecuteTime = curTime;
+			(*taskList[i].functionPointer)();
+		}
+	}
+}
+
+
+// Copy one Tasks array to another
+void FC_SimpleTasker::copyTaskList(Task *from, Task *to, uint8_t amount)
+{
+	for (int i=0; i < amount; i++)
+	{
+		to->functionPointer = from->functionPointer;
+		to->interval = from->interval;
+		to->maxDuration = from->maxDuration;
+		to->lastExecuteTime = from->lastExecuteTime;
+		to->timeShift = from->timeShift;
+	}
+}
+
+/*
+bool FC_SimpleTasker::checkIfContain(Task** source, int amt, Task* toCheck)
+{
+	for (int i=0; i<amt; i++)
+	{
+		if (source[i]->functionPointer == toCheck->functionPointer)
+			return true;
+	}
+	return false;
+}
+*/
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////
+//                FC_Tasker        \/  \/  \/
+//////////////////////////////////////////////////////////////////////////
+
+
+volatile bool FC_Tasker::baseLoopFlag = false;
+
+// Now do not used
+//volatile uint32_t FC_Tasker::baseLoopCounter = 0;
+//volatile uint32_t FC_Tasker::mainLoopCounter = 0;
+
+
+void baseLoopTimerHandler()
+{
+	FC_Tasker::baseLoopFlag = true;
+	//FC_Tasker::baseLoopCounter++; // increment the counter
+	//if ((FC_Tasker::baseLoopCounter % 4) == 0) FC_Tasker::mainLoopCounter++; // ONLY IF MAIN LOOP IS 250Hz AND BASE_INTERVAL IS 500 !!!!!!!!!!! - this part have to be implemented in a better way 
+}
+
+
+FC_Tasker::FC_Tasker( void (*mainFuncPointer)(), long interv, uint16_t maxDur ) : BASE_INTERVAL(interv), FC_SimpleTasker()
+{
+	/*
+	Timer2.setMode(TIMER_CH1, TIMER_OUTPUTCOMPARE);
+	Timer2.setPeriod(BASE_INTERVAL); // in microseconds
+	Timer2.setCompare(TIMER_CH1, 1);      // overflow might be small ???
+	Timer2.attachInterrupt(TIMER_CH1, baseLoopTimerHandler);
+	*/
+	
+	mainTask.functionPointer = mainFuncPointer;
+	mainTask.interval = interv;
+	mainTask.maxDuration = maxDur;
+	
+	timer2 = new HardwareTimer(2); // Hardware Timer 2 (STM32 BluePill)
+	timer2->pause();
+	timer2->setPeriod(BASE_INTERVAL);
+	timer2->setChannel1Mode(TIMER_OUTPUT_COMPARE);
+	timer2->setCompare(TIMER_CH1, 1);
+	timer2->attachCompare1Interrupt(baseLoopTimerHandler);
+	timer2->refresh();
+	timer2->resume();
+}
+
+
+/* - now in the constructor
 // Executed once. Add the main task
 void FC_Tasker::addMainFunction( void (*mainFuncPointer)(), long interv, uint16_t maxDur )
 {
@@ -61,24 +163,39 @@ void FC_Tasker::addMainFunction( void (*mainFuncPointer)(), long interv, uint16_
 	mainTask.interval = interv;
 	mainTask.maxDuration = maxDur;
 }
+*/
+
+
 
 
 void FC_Tasker::runTasker()
 {
-	//....
-	// if (baseLoopCounter == ...) then execute main function, at the end check if counter rised and if so, it means that system is unstable
-}
-
-
-// Copy one Tasks array to another
-void FC_Tasker::copyTaskList(Task *from, Task *to, uint8_t amount)
-{
-	for (int i=0; i < amount; i++)
+	if (baseLoopFlag)
 	{
-		to->functionPointer = from->functionPointer;
-		to->interval = from->interval;
-		to->maxDuration = from->maxDuration;
+		mainTask.lastExecuteTime = micros();
+		
+		// execute the main task
+		(*mainTask.functionPointer)();
+		
+		mainTaskDuration = micros() - mainTask.lastExecuteTime;
+		
+		// If this flag will be true -> that means system is overloaded
+		if (baseLoopFlag)
+		{
+			flag.systemOverloaded = true;
+			flag.systemUnstable = true;
+		}
+		else flag.systemOverloaded = true;
+		
+		baseLoopFlag = false;
 	}
+	
+	
+	// Running other tasks
+	/*
+		Reszte zadan ma byc wywolywane na podstawie czasu jaki uplynal od ostatniego wykonania, uzywajac micros() i dodajac shift
+	*/
+	
+	FC_SimpleTasker::runTasker();
 }
-
 
--- libraries/FC_Tasker_example/FC_Tasker_example.ino
+++ libraries/FC_Tasker_example/FC_Tasker_example.ino
@@ -4,25 +4,42 @@
 // Example file of FC_Tasker usage.
 //
 
-#include "FC_Tasker.h"
+/*
+	READ THAT!
+	Using FC_Tasker, after upload you need to restart the processor!
+*/
 
-FC_Tasker Tasker; // or Tasker( base interval )
-// ! Base interval is something like Tasker time resolution !
+#include "FC_Tasker.h"
 
 void func1(); // main
 void func2();
+void func3();
+
+
+// !!! Only one: FC_SimpleTasker or FC_Tasker can be used at the same time !!! (when using Simple, constructor have no parameters)
+//FC_SimpleTasker() sTasker;
+FC_Tasker tasker(func1, 500000L, 15); // main function, 2Hz
+
 
 
 void setup()
 {
-	Tasker.addMainFunction(func1, 4000L, 10); // 250 Hz
-	Tasker.addFunction(func2, 500000L, 15); // 2 Hz
+	Serial.begin(115200);
+	Serial.println("Program has started!");
+	
+	tasker.addFunction(func2, 1000000L, 15); // 1 Hz
+	tasker.addFunction(func3, 1000000L, 15); // 1 Hz
+	// add other functions
+	
+	tasker.scheduleTasks();
+	
+	Serial.println("End on setup!");
 }
 
 
 void loop()
 {
-	Tasker.runTasker();
+	tasker.runTasker();
 }
 
 
@@ -30,9 +47,17 @@ void loop()
 void func1()
 {
 	// Function 1 - main
+	Serial.println(" F1");
 }
 
 void func2()
 {
 	// Function 2
+	Serial.println("F2 test message");
+}
+
+void func3()
+{
+	// Function 3
+	Serial.println("F3 -----");
 }
