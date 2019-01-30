8f45f9cfdb4c8ecca518856b5cd1f1843e382ac9
Janek <jan.wielgus12@gmail.com>
2019-01-30 12:15:30 +0100

    First files of Communication_Base class

---
+++ libraries/FC_Communication_Base/FC_Communication_Base.cpp
@@ -0,0 +1,60 @@
+// Author: Jan Wielgus
+// Date: 29.01.2019
+//
+
+#include "FC_Communication_Base.h"
+
+
+// Trigger pSerial.update() and check if packet have been missed
+void FC_Communication_Base::receiveData()
+{
+	dataHandledFlag = false;
+	
+	pSerial.update(); // dataHandledFlag will be changed there if have been packer
+	
+	if (dataHandledFlag)
+		missedPackets=0;
+	else
+		missedPackets++;
+}
+
+
+// update max amt of lost packets to change comState to false
+void FC_Communication_Base::setMaxLostPackets(uint8_t maxLost)
+{
+	MaxLostPackets = maxLost;
+}
+
+
+// return communication state
+bool FC_Communication_Base::comState()
+{
+	if (missedPackets >= MaxLostPackets)
+		return false;
+	return true;
+}
+
+
+
+bool FC_Communication_Base::checkChecksum(const uint8_t* buf, size_t packerSize)
+{
+	uint8_t checksum = buf[1];
+	for (int i=2; i<packerSize; i++)
+		checksum ^= buf[i]; // xor'owanie kolejnych bajtow
+		
+	if (checksum == buf[0])
+		return true;
+	return false;
+}
+
+
+
+uint8_t FC_Communication_Base::calcChecksum(const uint8_t buf, size_t packerSize)
+{
+	uint8_t checksum = buf[1];
+	for (int i=2; i<packerSize; i++)
+		checksum ^= buf[i]; // xor'owanie kolejnych bajtow
+	
+	return checksum;
+}
+
---
+++ libraries/FC_Communication_Base/FC_Communication_Base.h
@@ -0,0 +1,46 @@
+// FC_Communication_Base.h
+//
+// Author: Jan Wielgus
+// Date: 29.01.2019
+//
+
+#ifndef _FC_COMMUNICATION_BASE_h
+#define _FC_COMMUNICATION_BASE_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#include <PacketSerial.h>
+
+
+
+class FC_Communication_Base
+{
+ public:
+	FC_Communication_Base(Stream* serial);
+	virtual void sendData(uint8_t pcktType) = 0;
+	void receiveData();
+	bool comState();
+	void setMaxLostPackets(uint8_t maxLost);
+	
+
+ protected:
+	virtual void handleReceivedData() = 0; // prev odbierzPriv()
+	PacketSerial pSerial;
+	Stream* comSerial;
+	bool checkChecksum(const uint8_t* buf, size_t packerSize); // xor'owanie
+	uint8_t calcChecksum(const uint8_t buf, size_t packerSize);
+	
+
+ private:
+	uint8_t MaxLostPackets=0;
+	bool dataHandledFlag; // to check if it was changed on true in handleReceivedData (if any data packet have been decoded)
+	uint16_t missedPackets = 0;
+	
+};
+
+#endif
+
