a1b8bc28e0c651abae4d0464a8b37f84b5bde393
Janek <jan.wielgus12@gmail.com>
2019-05-30 23:43:29 +0200

    Some development and general idea

--- libraries/FC_Communication_PacketHandler_example/FC_Communication_PacketHandler.cpp
+++ libraries/FC_Communication_PacketHandler_example/FC_Communication_PacketHandler.cpp
@@ -4,10 +4,18 @@
 
 #include "FC_Communication_PacketHandler.h"
 
-void FC_Communication_PacketHandler::init()
+
+FC_Communication_PacketHandler::FC_Communication_PacketHandler(Stream* serial, uint8_t bufSize)
+	:FC_Communication_Base(serial, bufSize)
 {
+	
+}
 
 
+
+void FC_Communication_PacketHandler::getReceivedData(receivedGardenData& dataContainer)
+{
+	dataContainer = allData;
 }
 
 
--- libraries/FC_Communication_PacketHandler_example/FC_Communication_PacketHandler.h
+++ libraries/FC_Communication_PacketHandler_example/FC_Communication_PacketHandler.h
@@ -13,10 +13,40 @@
 
 
 
-class FC_Communication_PacketHandler
+class FC_Communication_PacketHandler : public FC_Communication_Base
 {
  public:
-	void init();
+	FC_Communication_PacketHandler::FC_Communication_PacketHandler(Stream* serial, uint8_t bufSize);
+ 
+	struct receivedGardenData
+	{
+		uint8_t var1;
+		int liczba;
+		float zmienna;
+		int innaLiczba;
+	};
+	
+	struct gardenDataToSend
+	{
+		float temp;
+		int zmiennaDoWyslania;
+		uint8_t otherVar;
+	};
+
+
+	// Tu musisz sobie przypomniec jak nadpisac stara metode z klasy podstawowej ale uzyc starej metody we wnetrzu nowej
+	// W starej bedzie trzeba dopisac virtual lub zrobic inna nazwe (lub chyba sie da uzyc takiej samej a starej uzywac klasa::metoda() )
+	
+	// metody: odbieranie, wysylanie, sprawdzanie stanu polaczenia (moze to dac do podstawowej??!), przekazanie spakowanej paczki do wyslania
+	
+	void getReceivedData(receivedGardenData&); // returns reference to the structure of data to save time
+	
+	
+	
+ private:
+	
+	receivedGardenData receivedData; // this example data is private for this class and can be accessed through the getData() method
+	gardenDataToSend dataToSend; // powrorz sobie jak zrobic, zeby odebrane dane byly read-only dla outside a w dataToSend dalo sie wpisac dane przez jakas metode (przeslac gotowa paczke przez referencje)
 };
 
 
