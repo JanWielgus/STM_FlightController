5a27c09e7bd992bcc9b750bb0a87d0376fa0d1bc
JanWielgus <jan.wielgus12@gmail.com>
2019-05-30 14:02:34 +0200

    newLib/FC_Communication base (#6)
    * First files of Communication_Base class
    
    * Next few changes
    
    * Next improves in code, way of data handling have changed
    
    Now whole PacketSerial library is build in Communication_Base class.
    It don't need any derivative class to handle data (except for radio modules communication).
    
    * Everything put inside the Arduino project and lots of changes inside
    
    * Code for bug fixing
    
    * Bug fixed, now code is compilable and ready for testing
    
    * Some next improvements and bug fixes
    
    * Update FC_Communication_Base.cpp
    
    * Checksum methods changes
    
    * Update FC_Communication_Base_example.cppproj
    
    * Update FC_Communication_Base_example.ino

---
+++ libraries/FC_Communication_Base_example/FC_Communication_Base.cpp
@@ -0,0 +1,101 @@
+// Author: Jan Wielgus
+// Date: 29.01.2019
+//
+
+#include "FC_Communication_Base.h"
+
+
+FC_Communication_Base::FC_Communication_Base(Stream* serial, uint8_t bufSize): BufferSize(bufSize)
+{
+	this->serial = serial;
+	
+	// packet to prepare is used outside the class to pack there data to send
+	dpToSend.buffer = new uint8_t[bufSize];
+	dpReceived.buffer = nullptr; // pointer to this array is set after successful receiving
+	
+	// allocate memory for receive and decode buffer
+	receiveBuffer = new uint8_t[BufferSize];
+	decodeBuffer = new uint8_t[BufferSize];
+}
+
+FC_Communication_Base::~FC_Communication_Base()
+{
+	delete [] dpToSend.buffer;
+	delete [] receiveBuffer;
+	delete [] decodeBuffer;
+}
+
+
+// send data from dpToSend filled before outside the com class
+void FC_Communication_Base::sendData()
+{
+	if (dpToSend.buffer==0 || dpToSend.size==0)
+		return;
+	
+	uint8_t* encodeBuffer = new uint8_t[COBS::getEncodedBufferSize(dpToSend.size)];
+	
+	size_t numEncoded = COBS::encode(dpToSend.buffer, dpToSend.size, encodeBuffer);
+	
+	serial->write(encodeBuffer, numEncoded);
+	serial->write(PacketMarker);
+	
+	delete [] encodeBuffer;
+}
+
+
+
+bool FC_Communication_Base::receiveData()
+{
+	bool receivedDataFlag = false;
+	while (serial->available() > 0)
+	{
+		uint8_t data = serial->read();
+		
+		if (data == PacketMarker)
+		{
+			size_t numDecoded = COBS::decode(receiveBuffer, receiveBufferIndex, decodeBuffer);
+			
+			dpReceived.buffer = decodeBuffer;
+			dpReceived.size = numDecoded;
+			receivedDataFlag = true;
+			
+			receiveBufferIndex = 0;
+		}
+		else
+		{
+			if ((receiveBufferIndex + 1) < BufferSize)
+				receiveBuffer[receiveBufferIndex++] = data;
+			else
+			{
+				// ERROR, buffer oberflow if we write.
+			}
+		}
+	}
+	
+	return receivedDataFlag;
+}
+
+
+
+bool FC_Communication_Base::checkChecksum()
+{
+	uint8_t checksum = dpReceived.buffer[1];
+	for (int i=2; i<dpReceived.size; i++)
+		checksum ^= dpReceived.buffer[i]; // xor'owanie kolejnych bajtow
+		
+	if (checksum == dpReceived.buffer[0])
+		return true;
+	return false;
+}
+
+
+
+uint8_t FC_Communication_Base::calcChecksum()
+{
+	uint8_t checksum = dpToSend.buffer[1];
+	for (int i=2; i<dpToSend.size; i++)
+		checksum ^= dpToSend.buffer[i]; // xor'owanie kolejnych bajtow
+	
+	return checksum;
+}
+
---
+++ libraries/FC_Communication_Base_example/FC_Communication_Base.h
@@ -0,0 +1,66 @@
+// FC_Communication_Base.h
+//
+// Author: Jan Wielgus
+// Date: 29.01.2019
+//
+
+#ifndef _FC_COMMUNICATION_BASE_h
+#define _FC_COMMUNICATION_BASE_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#include <Arduino.h>
+#include "Encoding/COBS.h"
+//#include "Encoding/SLIP.h" // alternative
+
+/*
+typedef uint8 uint8_t;
+typedef uint16 uint16_t;
+typedef uint8 size_t;
+*/
+
+
+
+
+struct dataPacket
+{
+	uint8_t* buffer;
+	size_t size;
+};
+
+
+
+class FC_Communication_Base
+{
+ public:
+	const size_t BufferSize; // MAX: 256
+	//typedef void (*PacketHandlerFunction)(const uint8_t* buffer, size_t size);
+
+	FC_Communication_Base(Stream* serial, uint8_t bufSize=255); // serial, packetToPrepare - packet used outside to send data (there memory is allocated), bufSize - max buffer size
+	~FC_Communication_Base();
+	
+	void sendData(); // data to send packet before to dpToSend
+	bool receiveData(); // return false if there are no data
+	//bool isAvailable(); // receiveData() return true if was available and false if not
+	
+	// dataPackets to transfer data between class and outside
+	dataPacket dpToSend; // data packet used to send data (filled outside)
+	dataPacket dpReceived; // data packet with received data (used outside to unpack data)
+
+	bool checkChecksum(); // xor'owanie
+	uint8_t calcChecksum();
+	
+
+ private:
+	uint8_t* receiveBuffer;
+	uint8_t* decodeBuffer;
+	size_t receiveBufferIndex = 0;
+	uint8_t PacketMarker = 0;
+	Stream* serial;
+};
+
+#endif
