f7802381c5e6d58f3f03acbf84e5db7faae4cda4
Janek <jan.wielgus12@gmail.com>
2019-05-30 23:05:31 +0200

    Set up the project

---
+++ FlightController_v2_0/FlightController_v2_0.ino
@@ -0,0 +1,24 @@
+/*
+    Name:       FlightController_v2_0.ino
+    Created:	30/05/2019 22:12:18
+    Author:     Jan Wielgus
+	
+	Source code of second version of the FlightController on the STM BluePill microcontroller.
+*/
+
+
+
+
+void setup()
+{
+
+
+}
+
+
+
+void loop()
+{
+
+
+}
