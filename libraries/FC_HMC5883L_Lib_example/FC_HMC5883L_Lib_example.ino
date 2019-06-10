0df472e50f80e116c5ffd2a24fc7ae250d4b69ef
JanWielgus <jan.wielgus12@gmail.com>
2019-06-10 11:58:56 +0200

    newLib/FC_HMC5883L_Lib (#12)
    * Set up the project
    
    * First version ready for testing
    
    * Small bug fixes before testing
    
    * Added methods to enable HMC5883L module on GY86 board
    
    * Some new features
    
    - Method to enable Wire fast clock
    - Two new messages while debugging
    - One bug fix by new feature
    
    * Problems with compass solved and other bug fixes
    
    - Calibration has been tested
    - Ready to test tilting with data from MPU
    
    * Update FC_HMC5883L_Lib_example.cppproj

---
+++ libraries/FC_HMC5883L_Lib_example/FC_HMC5883L_Lib_example.ino
@@ -0,0 +1,45 @@
+/*
+    Name:       FC_HMC5883L_Lib_example.ino
+    Created:	06/06/2019 14:05:19
+    Author:     Jan Wielgus
+*/
+
+#include "FC_HMC5883L_Lib.h"
+
+
+FC_HMC5883L_Lib compass;
+
+
+
+void setup()
+{
+	Serial.begin(115200);
+	Serial.println("Program has just started!");
+	
+	Serial.println("Started enabling");
+	compass.enableHMC_on_MPU(); // IMPORTANT if use GY-86. If not then comment
+	Serial.println("ended");
+	
+	while (!compass.initialize())
+	{
+		// If program is there, some problems occured
+		Serial.println("HMC5883L cannot be initialized!");
+		delay(500);
+	}
+	
+	compass.calibrateCompass();
+	
+	compass.setCompassDeclination(5.0);
+}
+
+
+void loop()
+{
+	compass.readCompassData(0.0, 0.0);
+	
+	Serial.print("Heading: ");
+	Serial.print(compass.getHeading());
+	Serial.println();
+	
+	delay(14); // ~75Hz
+}
