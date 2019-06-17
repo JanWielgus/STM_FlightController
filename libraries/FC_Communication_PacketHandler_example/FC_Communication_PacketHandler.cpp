4813b48ffe03bb8af57078746d6513f5041a1818
Janek <jan.wielgus12@gmail.com>
2019-06-17 13:22:13 +0200

    Comment update

--- libraries/FC_Communication_PacketHandler_example/FC_Communication_PacketHandler.cpp
+++ libraries/FC_Communication_PacketHandler_example/FC_Communication_PacketHandler.cpp
@@ -12,13 +12,27 @@ FC_Communication_PacketHandler::FC_Communication_PacketHandler(Stream* serial, u
 }
 
 
+/*
+	SOME IMPORTANT INFO:
+	
+		- How to use checksums:
+		- buffer[0] in dataPacket is reserved for the checksum value
+		- if you are calculating the checksum, store it in the buffer[0]
+		  (calculate it after packing data to dataPacket!)
+		
+		- buffer[1] is reserved for data type (ID) !
+		- buffer[0] is not required for checksum if you do not use it
+		- buffer[1] is not required for data type (ID) if you use only one data packet type
+*/
+
+
 bool FC_Communication_PacketHandler::receiveAndUnpackData()
 {
 	// Stop if any data packet was received
 	if (!receiveData())
 		return false;
 	
-	
+	if (/* write checking method in base class */)
 	
 	
 	
