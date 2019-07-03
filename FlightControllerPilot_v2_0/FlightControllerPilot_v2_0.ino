8d6584f801a767423550c2b92f01d0a604e8acb6
Janek <jan.wielgus12@gmail.com>
2019-07-03 12:10:17 +0200

    Disarming is ready and tested

--- FlightControllerPilot_v2_0/FlightControllerPilot_v2_0.ino
+++ FlightControllerPilot_v2_0/FlightControllerPilot_v2_0.ino
@@ -214,14 +214,81 @@ void gestureRecognition()
 				}
 			}
 			
-			
-			
-			
+			// Arming end
+			//////////////////
 		}
+		
 		else if (state == armed)
 		{
 			//////////////////
 			// Disarming
+			//////////////////
+			
+			// Step 1
+			if (step1Passed == false)
+			{
+				step2Passed = false;
+				step3Passed = false;
+				
+				// Idle position
+				if (thr==0 && rot==0 && tb==0 && lr==0)
+					step1Passed = true;
+			}
+			
+			// Step 2
+			if (step1Passed && step2Passed == false)
+			{
+				// Update step start counter to make time limit
+				if (rot > -20)
+					stepStartCounter = counter;
+				
+				// Failure
+				if (thr > 5 || tb!=0 || lr!=0 || counter-stepStartCounter>6)
+					step1Passed = false;
+				
+				// Step 3 detection
+				if (rot < -450)
+				{
+					// Step 2 passed
+					step1Passed = true;
+					step2Passed = true;
+					stepStartCounter = counter;
+				}
+			}
+			
+			// Step 3
+			if (step1Passed && step2Passed && step3Passed == false)
+			{
+				// Failure
+				if (rot > -450 || thr > 5 || tb !=0 || counter-stepStartCounter>10)
+					step1Passed = false;
+				
+				// Step 4 detection
+				if (lr > 450)
+				{
+					step1Passed = true;
+					step2Passed = true;
+					step3Passed = true;
+					stepStartCounter = counter;
+				}
+			}
+			
+			// Step 4 - back to idle
+			if (step3Passed)
+			{
+				// Failure
+				if (counter-stepStartCounter > 7)
+					step1Passed = false;
+				
+				// Armed detection
+				if (thr==0 && rot==0 && tb==0 && lr==0)
+				{
+					step1Passed = false; // reset
+					state = disarmed;
+				}
+			}
+			
+			// Disarming end
 			//////////////////
 		}
 	}
