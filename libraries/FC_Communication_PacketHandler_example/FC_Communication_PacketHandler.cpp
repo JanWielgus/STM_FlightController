4c98e37217819c8126a9a83a8e1d22e29f2451d0
Janek <jan.wielgus12@gmail.com>
2019-08-10 12:57:51 +0200

    Added list of received data packets in the PacketHandler communication class

--- libraries/FC_Communication_PacketHandler_example/FC_Communication_PacketHandler.cpp
+++ libraries/FC_Communication_PacketHandler_example/FC_Communication_PacketHandler.cpp
@@ -9,6 +9,16 @@ FC_Communication_PacketHandler::FC_Communication_PacketHandler(Stream* serial, u
 	:FC_Communication_Base(serial, bufSize)
 {
 	conStab = 0;
+	
+	amtOfReceivedDataPackets = (sizeof(receivedPacketTypes) /* / sizeof(uint8_t) */)/2;
+	receivedDataPacketsList = new bool[amtOfReceivedDataPackets+1]; // +1 to use 1 to TYPE1 and so on...
+}
+
+
+FC_Communication_PacketHandler::~FC_Communication_PacketHandler()
+{
+	if (amtOfReceivedDataPackets > 0)
+		delete [] receivedDataPacketsList;
 }
 
 
@@ -28,10 +38,20 @@ FC_Communication_PacketHandler::FC_Communication_PacketHandler(Stream* serial, u
 
 
 
-bool FC_Communication_PacketHandler::receiveAndUnpackData()
+void FC_Communication_PacketHandler::beforeReceiving()
 {
 	// Need to be reset before receiving. Used to calculate connection stability.
 	atLeastOneFlag = false; // at least one packet was received. Needed to return true/false at the end
+	
+	// reset the list of received data packets
+	for (int i=1; i<=amtOfReceivedDataPackets; i++)
+		receivedDataPacketsList[i] = false;
+}
+
+
+bool FC_Communication_PacketHandler::receiveAndUnpackData()
+{
+	beforeReceiving();
 		
 	while (receiveData())
 	{
@@ -163,20 +183,37 @@ bool FC_Communication_PacketHandler::checkReceivedDataPacket(uint8_t packetID, u
 	
 	if (dpReceived.buffer[IDpos] == packetID && dpReceived.size == packetSize)
 	{
-		if (!checkChecksumFlag)
+		// if checkChecksumFlag == false or if checkChecksum==true if checkChecksum()==true
+		if ( !checkChecksumFlag || checkChecksum() )
 		{
-			atLeastOneFlag = true;
-			return true;
-		}
-		
-		//else
-		if (checkChecksum())
-		{
+			// check which packet was received
+			for (uint8_t i=0; i<amtOfReceivedDataPackets; i++)
+			{
+				uint8_t* address = ((uint8_t*)(&receivedPacketTypes)) + (i*2); // ID is every 2nd place in the receivedPacketTypes structure
+				if ( *address == packetID )
+				{
+					receivedDataPacketsList[i+1] = true;
+					break;
+				}
+			}
+			
+			
 			atLeastOneFlag = true;
 			return true;
 		}
 	}
 	
+	return false;
+}
+
+
+// One of the receivedPacketType structure elements can be passed only !!!!!!
+bool FC_Communication_PacketHandler::wasReceived(const uint8_t& packetID)
+{
+	uint8_t pos = (int)(&packetID) - (int)(&receivedPacketTypes) + 1; // number of checked type (TYPE1 = 1, TYPE2 = 2,... ONLY IF IN ORDER !!! )
+	
+	if (pos <= amtOfReceivedDataPackets)
+		return receivedDataPacketsList[pos];
 	return false;
 }
 
