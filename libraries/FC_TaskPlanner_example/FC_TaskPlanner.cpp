eeaf39607cddd2ded09ca117606ab19f5b3df42b
Janek <jan.wielgus12@gmail.com>
2019-09-10 21:40:52 +0200

    Created files for the TaskPlanner

---
+++ libraries/FC_TaskPlanner_example/FC_TaskPlanner.cpp
@@ -0,0 +1,11 @@
+/*
+    Name:       FC_TaskPlanner_example.ino
+    Created:	10/09/2019 21:32:39
+    Author:     Jan Wielgus
+*/
+// 
+
+#include "FC_TaskPlanner.h"
+
+
+
---
+++ libraries/FC_TaskPlanner_example/FC_TaskPlanner.h
@@ -0,0 +1,25 @@
+// FC_TaskPlanner.h
+/*
+    Name:       FC_TaskPlanner_example.ino
+    Created:	10/09/2019 21:32:39
+    Author:     Jan Wielgus
+*/
+
+#ifndef _FC_TASKPLANNER_h
+#define _FC_TASKPLANNER_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+class FC_TaskPlanner
+{
+	
+};
+
+
+#endif
+
