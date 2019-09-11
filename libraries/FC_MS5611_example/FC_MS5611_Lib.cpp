72c9ba31092c5295d69cdde2ea35ac413b88411e
Janek <jan.wielgus12@gmail.com>
2019-09-11 18:26:06 +0200

    Barometer program is almost done

--- libraries/FC_MS5611_example/FC_MS5611_Lib.cpp
+++ libraries/FC_MS5611_example/FC_MS5611_Lib.cpp
@@ -29,25 +29,28 @@ bool FC_MS5611_Lib::initialize(bool needToBeginWire_flag)
 	// Device setup
 		//For calculating the pressure the 6 calibration values need to be polled from the MS5611.
 		//These 2 byte values are stored in the memory location 0xA2 and up.
-	for (int i=0; i<6; i++)
+	for (int i=1; i<=6; i++)
 	{
 		Wire.beginTransmission(MS5611_Address);
-		Wire.write(0xA0 + (i+1)*2);
+		Wire.write(0xA0 + i*2);
 		Wire.endTransmission();
-		
+
 		Wire.requestFrom(MS5611_Address, 2);
 		C[i] = Wire.read() << 8 | Wire.read();
 	}
 	
 	// Pre-calculate some values
-	OFF_C2 = C[1] * pow(2, 16);
-	SENS_C1 = C[0] * pow(2, 15);
+	OFF_C2 = C[2] * pow(2, 16);
+	SENS_C1 = C[1] * pow(2, 15);
+	
 	
 	// The MS5611 needs a few readings to stabilize
-	for (int i=0; i<100; i++)
+	// Read pressure for 400ms
+	uint32_t readingEndTime = millis() + 400;
+	while (millis() < readingEndTime)
 	{
-		handleBaroData();
-		delay(4); // simulate 250Hz loop
+		// as fast as possible
+		runBarometer();
 	}
 }
 
@@ -58,38 +61,79 @@ void FC_MS5611_Lib::setFastClock()
 }
 
 
-void FC_MS5611_Lib::handleBaroData()
-{
-	
-}
-
 float FC_MS5611_Lib::getPressure()
 {
 	return pressure;
 }
 
 
+void FC_MS5611_Lib::runBarometer()
+{
+	taskPlanner.runPlanner();
+}
+
+
+
+
 void FC_MS5611_Lib::requestPressureFromDevice()
 {
-	
+	// Request pressure data
+	Wire.beginTransmission(MS5611_Address);
+	Wire.write(0x48);
+	Wire.endTransmission();
 }
 
 
 void FC_MS5611_Lib::getRawPressureFromDevice()
 {
-	
+	// Get the pressure data
+	Wire.beginTransmission(MS5611_Address);
+	Wire.write(0x00);
+	Wire.endTransmission();
+	Wire.requestFrom(MS5611_Address, 3);
+	rawPressure = Wire.read() << 16 | Wire.read() << 8 | Wire.read();
 }
 
 
 void FC_MS5611_Lib::requestTemperatureFromDevice()
 {
-	
+	// Request temperature data
+	Wire.beginTransmission(MS5611_Address);
+	Wire.write(0x58);
+	Wire.endTransmission();
 }
 
 
 void FC_MS5611_Lib::getRawTemperatreFromDevice()
 {
+	// Get the temperature data
+	Wire.beginTransmission(MS5611_Address);
+	Wire.write(0x00);
+	Wire.endTransmission();
+	Wire.requestFrom(MS5611_Address, 3);
+	rawTemperature = Wire.read() << 16 | Wire.read() << 8 | Wire.read();
+}
+
+
+void FC_MS5611_Lib::calculatePressureAndTemperatureFromRawData()
+{
+	// Calculate pressure as explained in the datasheet of the MS-5611.
+	dT = C[5];
+	dT <<= 8;
+	dT *= -1;
+	dT += rawTemperature;
+	OFF = OFF_C2 + ((int64_t)dT * (int64_t)C[4]) / pow(2, 7);
+	SENS = SENS_C1 + ((int64_t)dT * (int64_t)C[3]) / pow(2, 8);
+	intPressure = ((rawPressure * SENS) / pow(2, 21) - OFF) / pow(2, 15);
 	
+	pressure = (float)intPressure / 100;
+	
+	
+	///////////////
+	///
+	// Do some other calculations
+	///
+	///////////////
 }
 
 
--- libraries/FC_MS5611_example/FC_MS5611_Lib.h
+++ libraries/FC_MS5611_example/FC_MS5611_Lib.h
@@ -12,6 +12,7 @@
 #endif
 
 #include <Wire.h>
+#include <FC_TaskPlanner.h>
 
 
 class FC_MS5611_Lib
@@ -20,34 +21,38 @@ class FC_MS5611_Lib
 	FC_MS5611_Lib();
 	bool initialize(bool needToBeginWire_flag = true);
 	void setFastClock();
-	void handleBaroData(); // execute this function at 250Hz
 	float getPressure(); // new pressure value is about 80 times per second
-	
+	void runBarometer(); // called in the main loop() AS FAST AS POSSIBLE
 	
  private:
 	void requestPressureFromDevice();
 	void getRawPressureFromDevice(); // need to request first!
 	void requestTemperatureFromDevice();
 	void getRawTemperatreFromDevice(); // need to request first!
+	void calculatePressureAndTemperatureFromRawData(); // after requesting raw data
 	
 	
 	
 	
  private:
+	FC_TaskPlanner taskPlanner(2); // max 2 tasks will be planned at single moment
+	
 	static const uint8_t MS5611_Address = 0x77;
 	static const uint8_t AFTER_REQUEST_WAIT_TIME = 8;
 	
 	
 	// Device calibration values
-	uint16_t C[6];
-	int64_t OFF, OFF_C2, SENS, SENS_C1, P;
+	uint16_t C[7]; // 0 position is not used (7 to use positions from 1 to 6 as in the datasheet)
+	int64_t OFF, OFF_C2, SENS, SENS_C1;
+	int32_t dT;
 	
 	
 	
 	// readings
 	uint32_t rawPressure;
 	uint32_t rawTemperature;
-	float pressure;
+	int32_t intPressure; // (float)pressure * 100
+	float pressure; // intPressure / 100
 };
 
 
