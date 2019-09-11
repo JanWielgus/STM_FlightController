56e487ea02cf4813fc31eb0a3021bfec7b55043d
Janek <jan.wielgus12@gmail.com>
2019-09-11 14:08:41 +0200

    Example code is ready for testing

--- libraries/FC_TaskPlanner_example/FC_TaskPlanner_example.ino
+++ libraries/FC_TaskPlanner_example/FC_TaskPlanner_example.ino
@@ -9,13 +9,41 @@
 FC_TaskPlanner taskPlanner(5);
 
 
+void plannedTask1();
+void plannedTask2();
+
+
 void setup()
 {
+	Serial.begin(115200);
+	Serial.println("Program has been started!");
 	
+	taskPlanner.scheduleTask(plannedTask1, 2000); // call task1 in 2000ms (once)
+	Serial.println("Task1 has been planned to execute in 2 seconds");
 }
 
 
 void loop()
 {
+	taskPlanner.runPlanner();
+}
+
+
+
+void plannedTask1()
+{
+	Serial.println("Planned task 1 has been executed");
 	
+	// Plan second second task execution
+	int randomTime = random(1, 8) * 1000; // execute in between 1 and 8 seconds
+	taskPlanner.scheduleTask(plannedTask2, randomTime);
+	Serial.print("Task 2 has been planned to execute in ");
+	Serial.print(randomTime);
+	Serial.println(" seconds");
+}
+
+
+void plannedTask2()
+{
+	Serial.println("Planned task 2 has been executed");
 }
