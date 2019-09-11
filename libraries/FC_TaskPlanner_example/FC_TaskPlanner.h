249881e7bed20712e9bf7d4e221fb3775c64b58c
Janek <jan.wielgus12@gmail.com>
2019-09-11 12:01:11 +0200

    Library is ready

--- libraries/FC_TaskPlanner_example/FC_TaskPlanner.h
+++ libraries/FC_TaskPlanner_example/FC_TaskPlanner.h
@@ -14,10 +14,34 @@
 	#include "WProgram.h"
 #endif
 
+#include "Queue.h"
+
 
 class FC_TaskPlanner
 {
+ private:
+	typedef void (*functionPointer)();
 	
+ public:
+	FC_TaskPlanner(uint8_t plannedTasksMaxArrSize = 5); // max amt of planned tasks at one time
+	~FC_TaskPlanner();
+	bool scheduleTask(functionPointer fPtr, uint16_t call_in); // callIn - in how many milliseconds call a scheduled task; return true if added successfully
+	void runPlanner(); // execute this function in the main loop()
+	
+ private:
+	bool removeTaskFromArray(uint8_t taskPos); // used after function execution, remove empty space from the array (0 is the first task)
+	
+	
+ private:
+	const uint8_t MaxPlannedTasks;
+	
+	struct plannedTaskType
+	{
+		functionPointer functionPtr;
+		uint32_t timeToExecute;
+	};
+	plannedTaskType* plannedTasksArr; // array where stored are functions until they are called
+	uint8_t tasksInArray = 0; // amt of tasks in the array above
 };
 
 
