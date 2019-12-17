9824e185be4fee92acdcd491163010c35ae4930d
JanWielgus <jan.wielgus12@gmail.com>
2019-12-17 19:19:14 +0100

    LCD h file split to the h and cpp files and moved to a class
    And some other changes:
    - layout in excel
    - printing divided into two methods: fastUpdate and SlowUpdate for fast and slow changing values
    - other method to print once at the beginning static (not changed) texts

--- FlightControllerPilot_v2_0/LCDhandler.h
+++ FlightControllerPilot_v2_0/LCDhandler.h
@@ -9,94 +9,39 @@
 #ifndef LCDHANDLER_H_
 #define LCDHANDLER_H_
 
-#include "Storage.h"
+
+
+////////////////
+
+
+// I DON'T KNOW WHY THIS IS WORKING !!!
+// THERE SHOULD BE A CPP FILE WITH VARIABLE DECLARATION
+// IN H FILE SHOULD BE ONLY EXTERN OR NOTHING IF IT IS NOT USED !!!!
+// !!!!
+// !!!
+
+// THE SAME IS IN EVERY H FILE WITHOUT CPP FILE !!!
+
+//////////////
 
 
 
-class MesasureTime
+
+class LcdHandler
 {
-	public:
-	
-	static uint32_t startTime;
-	static uint32_t dur;
-	
-	public:
-	static void start()
-	{
-		startTime = micros();
-	}
-	
-	static void end()
-	{
-		dur = micros() - startTime;
-	}
-	
-	static uint16_t duration()
-	{
-		return dur;
-	}
+public:
+	LcdHandler();
+	void initLCD();
+	void showStaticParts(); // print on the LCD parts that do not change over time (like "Thr:"
+
+	void updateLCD_nonStaticParts(); // update fast and slow parts at the same time
+	// or
+	void updateLCD_fastParts(); // update fast-changing parts like throttle value
+	void updateLCD_slowParts();
+
+private:
+	void print(const String& str, uint8_t col, uint8_t row);
 };
-uint32_t MesasureTime::startTime = 0;
-uint32_t MesasureTime::dur = 0;
-
-uint16_t counter = 0; // Used to measure how many times function does in a second (checked in the updateControlDiode() )
-uint16_t temp_counter = 0;
-
-
-
-
-
-
-
-namespace lcdH // LCD handler
-{	
-	void initLCD()
-	{
-		lcd.init(); // Wire.begin() is here
-		lcd.backlight();
-		lcd.setCursor(0, 0);
-		lcd.print("FC Pilot");
-		lcd.setCursor(0, 1);
-		lcd.print("v 2.0");
-		delay(700);
-		lcd.clear();
-	}
-	
-	
-	void updateLCD()
-	{
-		// Print the throttle value
-		lcd.clear();
-		//lcd.setCursor(0, 0);
-		lcd.print("Thr: ");
-		lcd.print(thrStick.getValue());
-		
-		// print the state
-		lcd.setCursor(0, 1);
-		lcd.print("state: ");
-		switch (state)
-		{
-			case disarmed:
-			lcd.print("dsmd");
-			break;
-			case arming1:
-			lcd.print("a1");
-			break;
-			case arming2:
-			lcd.print("a2");
-			break;
-			case armed:
-			lcd.print("arm");
-			break;
-		}
-		
-		lcd.setCursor(10, 0);
-		//lcd.print(com.connectionStability());
-		//lcd.print(temp_counter);
-		//lcd.print(MesasureTime::duration());
-		lcd.print(btPID_P);
-	}
-}
 
 
 
