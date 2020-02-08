1114f7aa4087072d6b4145e8b1fdbb31744c6582
JanWielgus <jan.wielgus12@gmail.com>
2020-02-08 23:45:13 +0100

    Implementation of about half of the VirtualPilot

--- FlightController_v2_0/VirtualPilot.cpp
+++ FlightController_v2_0/VirtualPilot.cpp
@@ -8,17 +8,67 @@
 
 VirtualPilot::VirtualPilot()
 {
-	stabilizeFlightMode = new StabilizeFlightMode;
-	altHoldFlightMode = new AltHoldFlightMode(stabilizeFlightMode);
-	posHoldFlightMode = new PosHoldFlightMode(altHoldFlightMode);
+	// Create an array for all flight modes used by the VirtualPilot
+	amtOfFlightModes = 3;
+	flightModesArray = new flightModePackageType[amtOfFlightModes];
+
+	// Fill that array     ( id, layer, pointer, type )
+	fillFlightModesArrayRow(0, &stabilizeFlightMode, FlightModeType::STABILIZE);
+	fillFlightModesArrayRow(1, &altHoldFlightMode, FlightModeType::ALT_HOLD);
+	fillFlightModesArrayRow(2, &posHoldFlightMode, FlightModeType::POS_HOLD);
+	// NEXT FLIGHT MODES GOES HERE (remember to increase amtOfFlightModes!)
+
+
+	setFlightMode(FlightModeType::STABILIZE);
 }
 
 
 VirtualPilot::~VirtualPilot()
 {
-	delete stabilizeFlightMode;
-	delete altHoldFlightMode;
-	delete posHoldFlightMode;
+}
+
+
+
+void VirtualPilot::runVirtualPilot()
+{
+
+}
+
+
+void VirtualPilot::setFlightMode(FlightModeType flightModeToSet)
+{
+
+}
+
+
+FlightModeType VirtualPilot::getCurrentFlightMode()
+{
+	return currentFlightMode->type;
+}
+
+
+bool VirtualPilot::fillFlightModesArrayRow(uint8_t layer, FlightMode* ptr, FlightModeType type)
+{
+	// 'type' is the index in the array 
+
+	if ((uint8_t)type >= amtOfFlightModes)
+		return false;
+
+	flightModesArray[type].layer = layer;
+	flightModesArray[type].objPtr = ptr;
+	flightModesArray[type].type = type;
+	
+	return true;
+}
+
+
+FlightMode* VirtualPilot::getFlightModePtrByType(FlightModeType flightModeType)
+{
+	for (int i = 0; i < amtOfFlightModes; i++)
+	{
+		if (flightModeType == this->flightModesArray[i].type)
+			return this->flightModesArray[i].objPtr;
+	}
 }
 
 
--- FlightController_v2_0/VirtualPilot.h
+++ FlightController_v2_0/VirtualPilot.h
@@ -4,29 +4,52 @@
 #define _VIRTUALPILOT_h
 
 #include "arduino.h"
+#include "config.h"
 #include "FlightMode.h"
 #include "StabilizeFlightMode.h"
 #include "AltHoldFlightMode.h"
 #include "PosHoldFlightMode.h"
+#include "Storage.h"
 
 
 class VirtualPilot
 {
+private:
+	struct flightModePackageType
+	{
+		FlightModeType type;
+		FlightMode* objPtr; // flight mode pointer
+		uint8_t layer; // layer of the flight mode
+	};
+
+
 public:
 	VirtualPilot();
 	~VirtualPilot();
 
 	// public methods
-	// ..
+	void runVirtualPilot();
+	void setFlightMode(FlightModeType flightModeToSet);
+	FlightModeType getCurrentFlightMode();
+	
 
 
 private:
-	FlightMode* currentFlightMode; // pointer to the current flight mode (abstract type)
+	flightModePackageType* currentFlightMode; // pointer to the current flight mode (abstract type)
 
-	// Particular flight modes object pointers
-	StabilizeFlightMode* stabilizeFlightMode;
-	AltHoldFlightMode* altHoldFlightMode;
-	PosHoldFlightMode* posHoldFlightMode;
+	flightModePackageType* flightModesArray; // array of all available flight modes
+	uint8_t amtOfFlightModes = 0;
+
+	// Used flight modes
+	StabilizeFlightMode stabilizeFlightMode = StabilizeFlightMode();
+	AltHoldFlightMode altHoldFlightMode = AltHoldFlightMode(&stabilizeFlightMode);
+	PosHoldFlightMode posHoldFlightMode = PosHoldFlightMode(&altHoldFlightMode);
+	// other flight modes ...
+
+
+private:
+	bool fillFlightModesArrayRow(uint8_t layer, FlightMode* ptr, FlightModeType type);
+	FlightMode* getFlightModePtrByType(FlightModeType flightModeType);
 };
 
 
