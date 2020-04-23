1ce93e4163ccf2d77c91f5366e2dc84d0f503d0b
JanWielgus <jan.wielgus12@gmail.com>
2020-04-23 19:05:09 +0200

    New variable for only compass reading and some comments in other readings

--- FlightController_v2_0/SharedDataTypes.h
+++ FlightController_v2_0/SharedDataTypes.h
@@ -41,10 +41,11 @@ struct virtualSticksType
 
 struct readingsType
 {
-    FC_MPU6050Lib::vector3Float angle = { 0, 0, 0 }; // X and Y angles
-    float heading = 0;
+    FC_MPU6050Lib::vector3Float angle = { 0, 0, 0 }; // X and Y angles, 250Hz
+    float compassHeading; // extrapolated (250Hz)
+    float heading = 0; // 250Hz (fused or only gyro)
     float pressure = 0; // not extrapolated value (about 110 Hz)
-    float smoothPressure = 0;
+    float smoothPressure = 0; // 250Hz
 };
 
 
