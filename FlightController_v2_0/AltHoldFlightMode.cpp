a18424a487cca41e9e47721423a7a9fe37c0beaa
JanWielgus <jan.wielgus12@gmail.com>
2020-05-02 23:44:12 +0200

    Pressure to hold integral should be ready

--- FlightController_v2_0/AltHoldFlightMode.cpp
+++ FlightController_v2_0/AltHoldFlightMode.cpp
@@ -18,9 +18,10 @@ AltHoldFlightMode::AltHoldFlightMode(IFlightMode* stabilizeFM, IVirtualPilot* vi
 void AltHoldFlightMode::run()
 {
 	executeBaseFlightMode();
+	
 
-
-
+	// TODO: make altitude holding
+	// ...
 }
 
 
@@ -32,6 +33,9 @@ void AltHoldFlightMode::reset()
 
 	// Reset pid controller
 	Storage::altHoldPID.resetController();
+	
+	// reset the altHold base throttle
+	altHoldBaseThrottle = 0;
 }
 
 
@@ -39,6 +43,10 @@ void AltHoldFlightMode::reset()
 // can be called several times during working
 void AltHoldFlightMode::prepare()
 {
+	// TODO: Try to figure out better way of calculating base throttle for alt hold
+	// For now it is just the last throttle value given by the pilot
+	if (altHoldBaseThrottle == 0) // if not yet set
+		altHoldBaseThrottle = baseFlightMode->getVirtualSticks()->throttle;
 }
 
 
@@ -46,4 +54,15 @@ void AltHoldFlightMode::prepare()
 float AltHoldFlightMode::getPressureToHold()
 {
 	return pressureToHold;
+}
+
+
+void AltHoldFlightMode::integratePressureToHold()
+{
+	// Integrate only if connection is stable
+	if (Storage::comm.getConnectionStability() > 20)
+	{
+		float toIntegrate = (float)(baseFlightMode->getVirtualSticks()->throttle - altHoldBaseThrottle) / 50.f; // 6 pressure value change is about 0.5 meter
+		pressureToHold -= toIntegrate * config::MainDeltaTimeInSeconds;
+	}
 }
